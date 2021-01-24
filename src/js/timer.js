3cf711c049fa6967130154f5add1b6e67b4df553
AlexSytsenko <sytsenko@gmail.com>
2021-01-24 21:13:43 +0200

    created a timer based on user date

---
+++ src/js/timer.js
@@ -0,0 +1,63 @@
+
+
+class Countdown {
+    constructor(date, selector) {
+    
+        this.targetDate = new Date(date);
+        this.selector = selector;
+        this.anchorTag = document.querySelector(this.selector);
+        this.days = this.anchorTag.querySelector('[data-value="days"]');
+        this.hours = this.anchorTag.querySelector('[data-value="hours"]');
+        this.mins = this.anchorTag.querySelector('[data-value="mins"]');
+        this.secs = this.anchorTag.querySelector('[data-value="secs"]');
+    }
+    
+    startTimer() {
+        setInterval(() => {
+            const currentTime = Date.now();
+            const deltaTime = this.targetDate.getTime() - currentTime;
+
+            this.updateClockface(this.calculateTime(deltaTime)); 
+        }, 1000);
+    }
+       
+    updateClockface({ days, hours, mins, secs }) {
+        this.days.textContent = days;
+        this.hours.textContent = hours;
+        this.mins.textContent = mins;
+        this.secs.textContent = secs;
+    }
+
+    calculateTime(time) {
+        const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+        const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+        return ({ days, hours, mins, secs });
+    }
+
+    pad(value) {
+        return String(value).padStart(2, '0');
+    }
+}
+
+const title = document.querySelector('.title');
+
+const nameOfDay = prompt('Please enter the name of the future event');
+
+title.textContent = `${nameOfDay} will come in`;
+
+
+let userDate;
+
+for (let i = 1; i < 2; i += 1) {
+    userDate = prompt('Please enter the date in format 02-05-2021(month-day-year');  
+    if (userDate === ''|| userDate === null || isNaN(new Date(userDate).getTime()) === true || new Date(userDate).getTime() <= Date.now()) {
+        i -= 1;
+    }
+}
+
+const newYearTimer = new Countdown(userDate, "#timer-1");
+
+newYearTimer.startTimer();
